e044e38dba907a574e899affa63cbca643a199c4
Rajesh chandran <146159381+rajeshchandranaws1@users.noreply.github.com>
2024-03-04 12:20:30 +0530

    Update create-user.sh

--- TOOLS-INSTALLATION-SCRIPTS/ssh configuration/create-user.sh
+++ TOOLS-INSTALLATION-SCRIPTS/ssh configuration/create-user.sh
@@ -8,10 +8,3 @@ passwd $uname
 if [ $(grep -c "$uname ALL=(ALL) NOPASSWD: ALL" /etc/sudoers)  -eq 0 ]; then
 echo "$uname ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo;
 fi;
-
-sed -i 's/#PermitRootLogin[[:space:]]yes/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i 's/#PasswordAuthentication[[:space:]]yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication[[:space:]]no/#PasswordAuthentication no/' /etc/ssh/sshd_config
-sed -i 's/##PasswordAuthentication[[:space:]]no/#PasswordAuthentication no/' /etc/ssh/sshd_config
-
-/usr/bin/systemctl restart sshd.service
